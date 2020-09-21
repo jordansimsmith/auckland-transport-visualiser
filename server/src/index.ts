6fd8dcfe8021b1318eaf2382be150d31a7f1db16
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-09-21 17:13:01 +1200

    Implement routes endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,32 @@
 import express from 'express';
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const port = process.env.PORT || 5000;
+const apiKey = process.env.AT_API_KEY;
 
 const app = express();
 
-app.get('/', (_req, res) => {
-  res.send('Hello, World!');
+app.get('/routes', async (req, res) => {
+  const { shortName } = req.query;
+  if (!shortName) {
+    return res.status(400).send({ error: 'shortName query parameter missing' });
+  }
+
+  const url = `https://api.at.govt.nz/v2/gtfs/routes/routeShortName/${shortName}`;
+  try {
+    const { data } = await axios.get(url, {
+      headers: {
+        'Ocp-Apim-Subscription-Key': apiKey,
+      },
+    });
+
+    return res.status(200).send(data);
+  } catch (e) {
+    return res.status(502).send({ error: e.message });
+  }
 });
 
 app.listen(port, () => {
