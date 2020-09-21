075927bf09ce2c1af4017621b4d441c90a598368
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-09-21 17:42:09 +1200

    Implement locations endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -29,6 +29,42 @@ app.get('/routes', async (req, res) => {
   }
 });
 
+app.get('/locations', async (req, res) => {
+  const { routeId } = req.query;
+  if (!routeId) {
+    return res.status(400).send({ error: 'routeId query parameter missing' });
+  }
+
+  try {
+    const tripUrl = `https://api.at.govt.nz/v2/gtfs/trips/routeid/${routeId}`;
+    const { data: tripData } = await axios.get(tripUrl, {
+      headers: {
+        'Ocp-Apim-Subscription-Key': apiKey,
+      },
+    });
+
+    const tripIds = tripData.response.reduce(
+      (set: Set<string>, trip: any) => set.add(trip.trip_id),
+      new Set()
+    );
+
+    const locationsUrl = `https://api.at.govt.nz/v2/public/realtime/vehiclelocations`;
+    const { data: locationsData } = await axios.get(locationsUrl, {
+      headers: {
+        'Ocp-Apim-Subscription-Key': apiKey,
+      },
+    });
+
+    locationsData.response.entity = locationsData.response.entity.filter(
+      (entity: any) => tripIds.has(entity.vehicle?.trip?.trip_id)
+    );
+
+    return res.status(200).send(locationsData);
+  } catch (e) {
+    return res.status(502).send({ error: e.message });
+  }
+});
+
 app.listen(port, () => {
   console.log(`server started on http://localhost:${port}`);
 });
