944d1d304e987b2d48397a2248fdbf37ca2943d5
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-09-21 16:44:24 +1200

    Modify server to get port from environment

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-const PORT = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -8,6 +8,6 @@ app.get('/', (_req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`server started on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`server started on http://localhost:${port}`);
 });
